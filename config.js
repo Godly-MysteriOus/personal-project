561b0ec4382182fe631741af2432223231c51f7f
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2025-02-05 20:11:43 +0530

    introduced credential file to stop exposure of api keys onto github

---
+++ config.js
@@ -0,0 +1,9 @@
+require('dotenv').config({path:'./credential.env'});
+
+module.exports = {
+    devURI:process.env.devURI,
+    testURI:process.env.testURI,
+    prodURI:process.env.prodURI,
+    hostURI:process.env.hostURI,
+    emailAPI_key:process.env.emailAPI_KEY,
+}
