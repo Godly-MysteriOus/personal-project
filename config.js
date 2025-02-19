50c4ab9500ac4e4988a2d01b8fd5e6b67914f70c
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2025-02-19 14:41:00 +0530

    redis implementation

--- config.js
+++ config.js
@@ -1,10 +1,24 @@
 require('dotenv').config({path:'./credential.env'});
 
 module.exports = {
+    //connection URI
     devURI:process.env.devURI,
     testURI:process.env.testURI,
     prodURI:process.env.prodURI,
     hostURI:process.env.hostURI,
+
+    //email API key
     emailAPI_key:process.env.emailAPI_KEY,
+    
+    //google map API key
     googleMapAPIKey:process.env.googleMapAPIKey,
+
+    //cloudinary image storage API keys
+    cloudinaryCloudName:process.env.cloudinary_cloud_name,
+    cloudinaryApiKey:process.env.cloudinary_api_key,
+    cloudinaryApiSecret : process.env.cloudinary_api_secret,
+
+    //upstash redis key
+    upstashRedisUrl : process.env.upstash_redis_url,
+    upstasRedisToken : process.env.upstash_redis_token,
 }
