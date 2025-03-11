b43acff4bbb6d55090552f01527fe5236aeb3594
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2025-03-11 22:46:58 +0530

    more customer pages

--- routes/customerRoutes/customerRoutes.js
+++ routes/customerRoutes/customerRoutes.js
@@ -6,4 +6,8 @@ const customerController = require('../../controllers/userControllers/customerCo
 
 router.get('/homePage',authRoute.customerAuthentication,customerController.getHomePage);
 router.post('/homePage-search-product',authRoute.customerAuthentication,customerController.searchListedProducts);
+router.post('/user-locations',authRoute.customerAuthentication,customerController.userLocations);
+
+router.get('/profile',authRoute.customerAuthentication,customerController.getProfilePage);
+router.get('/cart',authRoute.customerAuthentication,customerController.getCartPage);
 module.exports = router;
