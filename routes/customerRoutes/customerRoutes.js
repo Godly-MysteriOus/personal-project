7cb9bc5cce827ba6f8cf7a40e2c10ef0851ce94c
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2025-03-11 21:02:42 +0530

    customer home page

--- routes/customerRoutes/customerRoutes.js
+++ routes/customerRoutes/customerRoutes.js
@@ -2,6 +2,8 @@ const app = require('express');
 const router = app.Router();
 const authRoute = require('../../middleware/authentication');
 const customerController = require('../../controllers/userControllers/customerController');
-router.get('/homePage',authRoute.customerAuthentication,customerController.getHomePage);
 
+
+router.get('/homePage',authRoute.customerAuthentication,customerController.getHomePage);
+router.post('/homePage-search-product',authRoute.customerAuthentication,customerController.searchListedProducts);
 module.exports = router;
