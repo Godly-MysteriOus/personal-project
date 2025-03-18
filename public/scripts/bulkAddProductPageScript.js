127ad7400af84b75385b43c2ec672cd330c4d4be
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2025-03-18 23:51:34 +0530

    seller side debounce logic added and made all product load data as reusable js

--- public/scripts/bulkAddProductPageScript.js
+++ public/scripts/bulkAddProductPageScript.js
@@ -5,57 +5,6 @@
 const addBtnSearchBox = document.querySelector('.addBtn');
 const gridContent = document.querySelector('.gridContent');
 let index = -1;
-// suggestions generate handler
-async function loadSuggestionData(val){
-    if(val.length<=0){
-        suggesstions.classList.add('suggestionBox-hidden');
-        return;
-    }
-    // retreives data from lcoalstorage for key
-    const suggesstionList = localStorage.getItem(keyName) ? JSON.parse(localStorage.getItem(keyName)) : [];
-    //filters data according to searchInput Data
-    let desiredSuggesstionList = suggesstionList.filter(item=>String(item?.name).toLowerCase().includes(val.toLowerCase()));
-    if(!desiredSuggesstionList || desiredSuggesstionList.length<5){
-        // make a api call to fetch and store data in local storage
-        const response = await fetch(url+'utils/searchProduct',{
-            method:'POST',
-            headers : { 'Content-Type': 'application/json' },
-            body:JSON.stringify({
-                medicineName: val,
-            }),
-        });
-        const result = await response.json();
-        if(!result.success){
-            message.textContent = result.message;
-            message.classList.remove('message-hidden');
-            setTimeout(()=>{
-                message.classList.add('message-hidden');
-            },3000);
-            return;
-        }
-        if(result.success && result.message=='Medicine Already Listed, will be updated !'){
-            alert('Medicine Already Listed, will be updated. Quantity will be added up');
-        }
-        localStorage.setItem(keyName,JSON.stringify(result.data));
-        //sorted data
-        desiredSuggesstionList =  result.data.filter(item => String(item.name).toLowerCase().includes(val.toLowerCase()));
-    }
-    // show 10 data to the UI
-    suggesstions.classList.remove('suggestionBox-hidden');
-    suggesstions.innerHTML = '';
-    const maxIteration = desiredSuggesstionList.length>10 ? 10 : desiredSuggesstionList.length;
-    for(let i=0; i<maxIteration; i++){
-        const option = document.createElement('option');
-        option.textContent = desiredSuggesstionList[i].name;
-        option.value = desiredSuggesstionList[i].productId;
-        option.classList.add('optionField');
-        suggesstions.appendChild(option);
-    }
-}
-searchInputField.addEventListener('input',async(e)=>{
-    const val = String(searchInputField.value);
-    loadSuggestionData(val);
-});
 function loadPage(){
     let listedProductsToAdd = localStorage.getItem(addedBulkProductKeyName);
     listedProductsToAdd = listedProductsToAdd ? JSON.parse(listedProductsToAdd) : [];
