04bb059bdda466417e221e08369d9e7afec697ea
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2025-03-19 19:59:40 +0530

    removed line2 and renamed line1 as address in addressInfo component, added active location section in customer Home Page

--- routes/customerRoutes/customerRoutes.js
+++ routes/customerRoutes/customerRoutes.js
@@ -14,5 +14,7 @@ router.get('/cart',authRoute.customerAuthentication,customerController.getCartPa
 // util routes
 router.get('/user-addresses',authRoute.customerAuthentication,customerController.getUserAddresses);
 router.post('/homePage-search-product',authRoute.customerAuthentication,customerController.searchListedProducts);
+router.get('/get-saved-locations',authRoute.customerAuthentication,customerController.getAllAddress);
+router.post('/update-active-address',authRoute.customerAuthentication,customerController.postUpdateActiveAddress);
 
 module.exports = router;
