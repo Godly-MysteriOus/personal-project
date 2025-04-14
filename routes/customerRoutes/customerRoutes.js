8fab040d0178159d0f7b91b336f7b1e5596a10d4
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2025-04-14 23:12:26 +0530

    cart page added

--- routes/customerRoutes/customerRoutes.js
+++ routes/customerRoutes/customerRoutes.js
@@ -8,6 +8,7 @@ router.get('/homePage',authRoute.customerAuthentication,customerController.getHo
 router.post('/save-address',authRoute.customerAuthentication,customerController.saveUserAddress);
 router.get('/profile',authRoute.customerAuthentication,customerController.getProfilePage);
 router.get('/cart',authRoute.customerAuthentication,customerController.getCartPage);
+router.get('/cart-products',authRoute.customerAuthentication,customerController.getCartProduct);
 router.post('/add-to-cart',authRoute.customerAuthentication,customerController.postAddToCart);
 
 
@@ -16,5 +17,7 @@ router.get('/user-addresses',authRoute.customerAuthentication,customerController
 router.post('/search-product',authRoute.customerAuthentication,customerController.searchListedProducts);
 router.get('/get-saved-locations',authRoute.customerAuthentication,customerController.getAllAddress);
 router.post('/update-active-address',authRoute.customerAuthentication,customerController.postUpdateActiveAddress);
-
+router.post('/quantity-add',authRoute.customerAuthentication,customerController.increaseProductQuantity);
+router.post('/quantity-reduce',authRoute.customerAuthentication,customerController.reduceProductQuantity);
+router.post('/delete-product',authRoute.customerAuthentication,customerController.deleteProduct);
 module.exports = router;
